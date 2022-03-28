f8074fc7989871ccfde7e96c42cc16ff5dad8843
hasan-mohammad-bd <hasan.md.east@gmail.com>
2022-03-28 09:50:17 +0600

    adding some info

---
+++ src/note.js
@@ -0,0 +1,28 @@
+/* 
+## 51-1: Module Introduction Use Bootstrap CDN in React
+
+bootstrap
+=================
+# bangla system to add bootstrap with react is copy the cdn to the index.html header section.
+#you have to change the class to className
+# to change the class: ctrl +f (class= to className=)
+# the image tag can be fix with self closing (/>) at the end of the tag.
+# dont repeat the card though react has component same look different in data.
+# you  can create a card component to make things easier.
+
+## 51-2 Install React Bootstrap in your react application
+
+# to install anything with react, search: react + which you would like to use.
+
+==============================================
+# command line: npm install react-bootstrap bootstrap@5.1.3 
+
+# copy the link to app.js or index.js : import 'bootstrap/dist/css/bootstrap.min.css
+
+# to change the or import component for bootstrap: for example: use capital letter of button as <Button which will be suggested as "react bootstrap"
+
+#every component must be imported from the react bootstrap.
+==============================================
+ 
+
+ */
