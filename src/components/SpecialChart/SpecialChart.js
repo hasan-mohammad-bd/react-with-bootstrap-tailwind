0bf2874c0ce3cbd7c750809ba72f35669c187e5f
hasan-mohammad-bd <hasan.md.east@gmail.com>
2022-03-30 12:39:19 +0600

    fixcing problem

--- src/components/SpecialChart/SpecialChart.js
+++ src/components/SpecialChart/SpecialChart.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 const SpecialChart = () => {
     const [phone, setPhone] = useState([])
